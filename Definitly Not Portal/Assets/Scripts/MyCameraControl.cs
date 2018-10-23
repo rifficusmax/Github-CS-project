ee7f62f8a35047624fa87b84fc3acc023e72f5d2
rifficusmax <rifficusmax@yahoo.com>
2018-10-23 14:24:49 -0400

    CS project
    CS project

---
+++ Definitly Not Portal/Assets/Scripts/MyCameraControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+// Script to control the camera via keyboard (arrow keys).
+public class MyCameraControl : MonoBehaviour
+{
+    [Tooltip("Velocity of the camera in meters per second limited to [ 0.1, 10 ].")]
+    [Range(0.1F, 10.0F)]
+    public float velocity = 0.1F; // Velocity of the camera [m/sec];
+
+    // Update is called once per frame.
+    void Update()
+    {
+        Vector3 currCamPos = transform.position; // Get current camera position.
+
+        // Track keyboard arrow keys, and update the current camera position.
+        if (Input.GetKey(KeyCode.LeftArrow)) { currCamPos.x -= Time.deltaTime * velocity; }
+        if (Input.GetKey(KeyCode.RightArrow)) { currCamPos.x += Time.deltaTime * velocity; }
+        if (Input.GetKey(KeyCode.UpArrow)) { currCamPos.z += Time.deltaTime * velocity; }
+        if (Input.GetKey(KeyCode.DownArrow)) { currCamPos.z -= Time.deltaTime * velocity; }
+
+        transform.position = currCamPos; // Update camera position.
+    }
+}
+
+
---
+++ Definitly Not Portal/Assets/Scripts/ShootingScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingScript : MonoBehaviour
+{
+
+    public GameObject projectilePrefab = null; // Reference variable to the cannon ball prefab.
+
+    public Transform spawnLocation = null; // Reference variable to the Transform component of the spawn location.
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetMouseButtonDown(0))
+        {
+
+            GameObject projectileClone = null;
+
+            Vector3 forceDir = spawnLocation.TransformDirection(Vector3.forward);
+            Quaternion rot = Quaternion.FromToRotation(Vector3.forward, forceDir);
+            projectileClone = Instantiate(projectilePrefab, spawnLocation.position, rot);
+
+            //Set Game object to active
+            projectileClone.SetActive(true);
+
+
+            Rigidbody projectileRigidbody = projectileClone.GetComponent<Rigidbody>();
+            projectileRigidbody.velocity = projectileRigidbody.transform.forward * 24;
+
+            Destroy(projectileClone, 5.0f);
+        }
+    }
+}
