332544923488ece66833c17c9bf5e2997d5bddd2
rifficusmax <rifficusmax@yahoo.com>
2018-10-30 16:48:09 -0400

    Stuff
    Stuff

--- Definitly Not Portal/Assets/Scripts/CreatePortal.cs
+++ Definitly Not Portal/Assets/Scripts/CreatePortal.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CreatePortal : MonoBehaviour {
+public class CreatePortal : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start ()
+    ContactPoint contact; //The location the projectile collided with
+    public GameObject portalObject;
+    public GameObject portalClone;
+
+    // Use this for initialization
+    void Start ()
     {
 		
 	}
@@ -13,15 +18,23 @@ public class CreatePortal : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-		
+
 	}
 
-    void OnTriggerEnter(Collider col)
+    void OnCollisionEnter(Collision collision)
     {
-        if(col.gameObject.name == "Projectile(Clone)")
+        if(collision.gameObject.tag == "InnerCol")
         {
-            Destroy(col.gameObject);
+            contact = collision.contacts[0]; //Grab contact point and store it in the collision structure
+            Quaternion rot = Quaternion.FromToRotation(Vector3.back, contact.normal); //Assign a rotation to it using the normal vector
+            Vector3 pos = contact.point; //Assign a vector3 position
+            portalClone = Instantiate(portalObject, pos, rot);//Create object
+            portalClone.SetActive(true);//Turn on the game object
+            Destroy(this.gameObject); //Destroy the bullet
         }
-       
+        else if(collision.gameObject.tag == "OuterCol") //Portal cannot be spawned
+        {
+            Destroy(this.gameObject); //Destroy the bullet
+        }
     }
 }
